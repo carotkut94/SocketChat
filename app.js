12a2733a0489370701c20a7e866444cd59c7f585
deathcode <carotkut12@gmail.com>
2019-01-20 05:52:43 -0800

    Added more actions on socket event

--- app.js
+++ app.js
@@ -1,31 +1,156 @@
 const express = require('express');
-
+const Request = require("request");
 http = require('http');
 app = express();
 server = http.createServer(app);
 
+let users = new Map();
+
 io = require('socket.io').listen(server);
 
-io.on('connection', (socket) =>{
-   console.log('user connected', socket.id) ;
-   socket.on('join', function (userName) {
-       console.log(userName+" has joined the chat");
-       socket.broadcast.emit('userjoinedthechat',userName+" has joined the chat");
-   });
-   socket.on('chat', async function(data) {
-       console.log(data)
-       io.sockets.emit('chat',data);
-   })
+io.on('connection', (socket) => {
 
-    socket.on('typing', async function(data) {
-        socket.broadcast.emit('typing', data);
-    })
+    /**
+     * 1. If user is chatting for the first time, then
+     * he/she should be added into the contact list
+     * 2. this is the server code, that handles the emission of
+     * messages/events and data, which is bidirectional
+     */
+
+    socket.on('chat', async function (data) {
+        // insert the message in the database
+        // emit the message
+        // and add the message in the view.
+        // io.to(users.get(data.to)).emit('chat', data);
+
+        Request.post({
+            "url": "http://localhost/temp_site/home/chat_Ins",
+            form: {
+                msg: data.message,
+                userID: data.to,
+                username: data.username,
+                user_id: data.from,
+                sentby: data.sentby
+            }
+        }, (error, response, body) => {
+            if (error) {
+                return console.dir(error);
+            }
+            console.log(body);
+            Request.post({
+                "url": "http://localhost/temp_site/home/get_socket_id",
+                form: {
+                    user_id: data.to,
+                }
+            }, (error, response, body) => {
+                if (error) {
+                    return console.dir(error);
+                }
+                io.to(body).emit('chat', data);
+                io.to(body).emit('active', data);
+                io.to(socket.id).emit('inserted', data.to);
+            });
+        });
+        // $.ajax({
+        //     data: {msg: msg, userID: usid, username: usname},
+        //     type: "post",
+        //     url: "http://localhost/tutordip/home/chat_Ins",
+        //
+        //     success: function (data) {
+        //         // console.log("data");
+        //         // alert("Data Save: " + data);
+        //     }
+        // });
+    });
+
+    socket.on('typing', async function (data) {
+        // If user starts typing then send the typing event from client to the
+        // chatting client
+        console.log(data.to);
+        console.log(data.from);
+        console.log(data.message);
+        Request.post({
+            "url": "http://localhost/temp_site/home/get_socket_id",
+            form: {
+                user_id: data.to,
+            }
+        }, (error, response, body) => {
+            if (error) {
+                return console.dir(error);
+            }
+            io.to(body).emit('typing', data);
+        });
+    });
+
+    socket.on('active', async function (data) {
+        // If user starts typing then send the typing event from client to the
+        // chatting client
+        console.log(data.to);
+        console.log(data.from);
+        Request.post({
+            "url": "http://localhost/temp_site/home/get_socket_id",
+            form: {
+                user_id: data.to,
+            }
+        }, (error, response, body) => {
+            if (error) {
+                return console.dir(error);
+            }
+            io.to(body).emit('active', data);
+        });
+    });
+
+
+    socket.on("register", async function (data) {
+        // Whenever a user gets connected he/she register via calling
+        // this event,
+        // on this even need to set the user as online
+        // and call the refresh event so that user can refresh the online user
+        // list
+        console.log(socket.id + "======>" + data);
+        users.set(data, socket.id);
+        socket.broadcast.emit("refresh", users);
+        Request.post({
+            "url": "http://localhost/temp_site/home/socket_status",
+            form: {
+                socket_id: socket.id,
+                user_id: data,
+                status: 'online'
+            }
+        }, (error, response, body) => {
+            if (error) {
+                return console.dir(error);
+            }
+            console.log(body);
+        });
+    });
+
+    socket.on('disconnect', async function (reason) {
+        // Whenever user is disconnected remove set user status in database
+        // and emit refresh event
+        console.log(reason);
+        console.log(socket.id);
+        socket.broadcast.emit("refresh", users);
+        Request.post({
+            "url": "http://localhost/temp_site/home/socket_offline",
+            form: {
+                socket_id: socket.id,
+                status: 'offline'
+            }
+        }, (error, response, body) => {
+            if (error) {
+                return console.dir(error);
+            }
+            console.log(body);
+        });
+    });
+
 });
 
-server.listen(3000, ()=>{
-   console.log('Node app is running on port 3000');
+server.listen(3000, () => {
+    console.log('Node app is running on port 3000');
 });
 
 
 // static file
-app.use(express.static('public'))
+app.use(express.static('public'));
--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
-var socket = io.connect('http://localhost:3000');
-
+let userName = prompt("Enter a username to register with");
+let socket = io.connect('http://localhost:3000');
 // DOM Selection
 let message = document.getElementById('message');
 let handle = document.getElementById('handle');
@@ -7,24 +7,53 @@ let btn = document.getElementById('send');
 let output = document.getElementById('output');
 let feedback = document.getElementById('feedback');
 
+handle.value = userName
+
 btn.addEventListener('click', async function () {
     socket.emit('chat', {
         message: message.value,
         handle: handle.value,
-        time: Date()
+        time: Date(),
+        to: prompt("Enter username to send in the message"),
+    });
+    output.innerHTML += `<p><strong>${handle.value}</strong> : ${message.value}<br>${Date()}</p>`;
+});
+
+message.addEventListener("keypress", async function () {
+    socket.emit('typing', {
+        message: userName+" is typing...",
+        to: prompt("Enter username to typing marker"),
     });
 });
 
-message.addEventListener('keypress', async function() {
-    socket.emit('typing', handle.value);
+socket.on('connect', async function () {
+    console.log("Connected");
+    socket.emit("register",{
+        key:userName,
+        value:socket.id
+    });
 });
+
+
 
 //Listen for events
 socket.on('chat', async function (data) {
-   output.innerHTML += `<p><strong>${data.handle}</strong> : ${data.message}<br>${data.time}</p>`;
+    output.innerHTML += `<p><strong>${data.handle}</strong> : ${data.message}<br>${data.time}</p>`;
     feedback.innerText = '';
 });
 
-socket.on('typing', async function(data) {
-   feedback.innerHTML = `<p><em><strong>${data}</strong> is typing a message</em></p>`;
+//Listen for typing event
+socket.on('typing', async function (data) {
+    feedback.innerHTML = `<p><strong>${data.message}</strong></p>`;
 });
+
+//Listen for refresh even
+socket.on('refresh', async function (data) {
+    console.log("refresh called");
+    for(let user in data){
+        if(data.hasOwnProperty(user)){
+            console.log(user.key + " Username " + user.value);
+        }
+    }
+});
+
